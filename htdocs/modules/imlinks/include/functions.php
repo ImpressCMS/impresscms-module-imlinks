559eab289b74fd5d7e76e36be52f57abb5655c30
algalochkin <contact@impresscms.org>
2008-11-13 19:54:39 +0000

    fix quote for _AM_IMLINKS_ICO_CLONE definition ;-)
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imlinks/trunk@6829 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- htdocs/modules/imlinks/include/functions.php
+++ htdocs/modules/imlinks/include/functions.php
@@ -706,7 +706,7 @@ function iml_linklistbody( $published ) {
     }
     $icon = '<a href="index.php?op=edit&amp;lid=' . $lid . '" title="' . _AM_IMLINKS_ICO_EDIT . '">' . $imagearray['editimg'] . '</a>&nbsp;';
     $icon .= '<a href="index.php?op=delete&amp;lid=' . $lid . '" title="' . _AM_IMLINKS_ICO_DELETE . '">' . $imagearray['deleteimg'] . '</a>&nbsp;';
-	$icon .= '<a href="index.php?op=clone&amp;lid=' . $lid . '" title=_AM_IMLINKS_ICO_CLONE>' . $imagearray['clone'] . '</a>&nbsp;';
+	$icon .= '<a href="index.php?op=clone&amp;lid=' . $lid . '" title="' . _AM_IMLINKS_ICO_CLONE . '">' . $imagearray['clone'] . '</a>&nbsp;';
     $icon .= '<a href="altcat.php?op=main&amp;cid="' . $cid . '&amp;lid=' . $lid . '&amp;title=' . $immyts -> htmlSpecialCharsStrip( trim( $published['title'] ) ) . '" title="' . _AM_IMLINKS_ALTCAT_CREATEF . '">' . $imagearray['altcat'] . '</a>&nbsp;';
     $icon .= '<a href="http://whois.domaintools.com/' . $hwhoisurl . '" target="_blank"><img src="' . ICMS_URL . '/modules/' . $xoopsModule -> getVar( 'dirname' ) . '/images/icon/domaintools.png" alt="WHOIS" title="WHOIS" align="absmiddle"/></a>';
 
