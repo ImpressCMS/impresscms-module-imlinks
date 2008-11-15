04d7e5eed856b2ea35b118b7f577cb5f64e0602a
Ronald <pietjebell31@hotmail.com>
2008-11-15 14:17:47 +0000

    Made changes/fixes to category banner
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imlinks/trunk/htdocs/modules/imlinks@6936 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- blocks/imlinks_banner.php
+++ blocks/imlinks_banner.php
@@ -4,7 +4,7 @@
 *
 * Based upon WF-Links 1.06
 *
-* File: wflinks_banner.php
+* File: blocks/imlinks_banner.php
 *
 * @copyright		http://www.xoops.org/ The XOOPS Project
 * @copyright		XOOPS_copyrights.txt
@@ -26,18 +26,18 @@
 * @version		$Id$
 */
  
-if ( !defined( 'ICMS_ROOT_PATH' ) ) { die( 'ICMS root path not defined' ); }
-
 function b_imlinks_banner_show( $options ) {
-    $mydirname = basename( dirname( __FILE__ ) );
-    global $xoopsDB;
+    
+	$mydirname = basename( dirname(  dirname( __FILE__ ) ) ) ;
+    
+	global $xoopsDB;
 
     $block = array();
     $time = time();
     $modhandler = &xoops_gethandler( 'module' );
-    $wflModule = &$modhandler -> getByDirname( $mydirname );
+    $imlModule = &$modhandler -> getByDirname( $mydirname );
 
-    $result = $xoopsDB -> query( "SELECT a.cid as acid, a.title, a.client_id, a.banner_id, b.bid, b.cid, b.imptotal, b.impmade, b.clicks FROM " . $xoopsDB -> prefix( 'imlinks_cat' ) . " a, " . $xoopsDB -> prefix( 'banner' ) . " b WHERE (b.cid = a.client_id) OR (b.bid = a.banner_id) ORDER BY b.cid, b.bid, a.title ASC" );
+    $result = $xoopsDB -> query( 'SELECT a.cid as acid, a.title, a.client_id, a.banner_id, b.bid, b.cid, b.imptotal, b.impmade, b.clicks FROM ' . $xoopsDB -> prefix( 'imlinks_cat' ) . ' a, ' . $xoopsDB -> prefix( 'banner' ) . ' b WHERE (b.cid = a.client_id) OR (b.bid = a.banner_id) ORDER BY b.cid, b.bid, a.title ASC' );
 
     while ( $myrow = $xoopsDB -> fetchArray( $result ) ) {
 
@@ -45,29 +45,33 @@ function b_imlinks_banner_show( $options ) {
         $clicks = $myrow['clicks'];
         $imptotal = $myrow['imptotal'];
         $bannerload = array();
-        $result2 = $xoopsDB -> query( "SELECT name FROM " . $xoopsDB -> prefix( 'bannerclient' ) . " WHERE cid=" . intval($myrow['cid']) );
+		
+        $result2 = $xoopsDB -> query( 'SELECT name FROM ' . $xoopsDB -> prefix( 'bannerclient' ) . ' WHERE cid=' . intval( $myrow['cid'] ) );
         $myclient = $xoopsDB -> fetchArray( $result2 );
+		
         if ( $impmade == 0 ) {
             $percent = 0;
         } else {
-            $percent = substr(100 * $clicks / $impmade, 0, 5);
+            $percent = substr( 100 * $clicks / $impmade, 0, 5 );
         }
+		
         if ( $imptotal == 0 ) {
             $left = 'Unlimited';
         } else {
-            $left = intval($imptotal-$impmade);
+            $left = intval( $imptotal - $impmade );
         }
-        $bannerload['cat'] = intval($myrow['acid']);
-        $bannerload['bid'] = intval($myrow['bid']);
-        $bannerload['cid'] = intval($myrow['cid']);
-        $bannerload['imptotal'] = intval($myrow['imptotal']);
-        $bannerload['impmade'] = intval($myrow['impmade']);
+		
+        $bannerload['cat'] = intval( $myrow['acid'] );
+        $bannerload['bid'] = intval( $myrow['bid'] );
+        $bannerload['cid'] = intval( $myrow['cid'] );
+        $bannerload['imptotal'] = intval( $myrow['imptotal'] );
+        $bannerload['impmade'] = intval( $myrow['impmade'] );
         $bannerload['impleft'] = $left;
-        $bannerload['clicks'] = intval($myrow['clicks']);
+        $bannerload['clicks'] = intval( $myrow['clicks'] );
         $bannerload['client'] = $myclient['name'];
         $bannerload['percent'] = $percent;
         $bannerload['cattitle'] = $myrow['title'];
-        $bannerload['dirname'] = $wflModule -> getVar( 'dirname' );
+        $bannerload['dirname'] = $mydirname;
         $block['banners'][] = $bannerload;
     }
     unset( $_block_check_array );
@@ -75,7 +79,7 @@ function b_imlinks_banner_show( $options ) {
 }
 
 function b_imlinks_banner_edit( $options ) {
-    $form = "";
+    $form = '';
     return $form;
 } 
 
