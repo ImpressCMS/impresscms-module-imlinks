b3101c1900382385d4482a10fd7b734bf4ddeee0
Ronald <pietjebell31@hotmail.com>
2008-11-26 20:39:26 +0000

    Changed fontsize
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imlinks/trunk/htdocs/modules/imlinks@7235 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- include/functions.php
+++ include/functions.php
@@ -455,8 +455,7 @@ function iml_displayimage( $image = '', $path = '', $imgsource = '', $alttext = 
 
 function iml_letters() {
     global $xoopsModule;
-//	$style = 'style="margin-bottom: 8px; margin-top: 8px; padding-right: 0.5em; padding-left: 0.5em; padding-bottom: 1px; padding-top: 1px; background-image: url(' . ICMS_URL . '/modules/' . $xoopsModule -> getVar( 'dirname' ) . '/images/icon/backgnd.png); background-position: center center; background-repeat: repeat-x; color: #444; font-size: smaller; font-weight: bold; cursor: pointer; border: outset 1px #ccc;"';
-    $letterchoice = '<div style="padding: 2px;">' . _MD_IMLINKS_BROWSETOTOPIC . '</div>';
+    $letterchoice = '<div style="padding: 2px; font-size: smaller;">' . _MD_IMLINKS_BROWSETOTOPIC . '</div>';
  //   $letterchoice .= '';
     $alphabet = array ( '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' );
     $num = count( $alphabet ) - 1;
