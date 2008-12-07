5b73c2daf53f61f9bd034313d3c40663986de0b9
Ronald <pietjebell31@hotmail.com>
2008-12-07 20:22:56 +0000

    Removed some old code from previous rating feature
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imlinks/trunk@7490 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- htdocs/modules/imlinks/singlelink.php
+++ htdocs/modules/imlinks/singlelink.php
@@ -244,8 +244,6 @@ if ( is_object( $xoopsUser ) && !empty( $xoopsUser ) ) {
   }
 }
 
-// $xoopsTpl -> assign ( 'ratethislink', '<a class="button" href="' . ICMS_URL . '/modules/' . $mydirname . '/ratelink.php?cid=' . $link_arr['cid'] . '&amp;lid=' . $link_arr['lid'] . '"><img src="' . ICMS_URL . '/modules/' . $mydirname . '/images/icon/medal_gold.png" alt="" style="vertical-align: middle;" /> ' ._MD_IMLINKS_RATETHISFILE . '</a>');
-
 $xoopsTpl -> assign ( 'reportbroken', '<a class="button" href="' . ICMS_URL . '/modules/' . $mydirname . '/brokenlink.php?lid=' . $link_arr['lid'] . '"><img src="' . ICMS_URL . '/modules/' . $mydirname . '/images/icon/link_break.png" alt="" style="vertical-align: middle;" /> ' . _MD_IMLINKS_REPORTBROKEN . '</a>');
 
 $xoopsTpl -> assign ( 'mailto', '<a class="button" href="mailto:?subject=' . $mail_subject . '&body=' . $mail_body . '" target="_top"><img src="' . ICMS_URL . '/modules/' . $mydirname . '/images/icon/email.png" alt="" style="vertical-align: middle;" /> ' . _MD_IMLINKS_TELLAFRIEND . '</a>');
