e8df3bc8540e13c56908c3fe9bb74de49496012a
Ronald <pietjebell31@hotmail.com>
2008-12-11 20:06:03 +0000

    Commented out support-info for About page until rel="external" is fixed in core.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imlinks/trunk@7582 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- htdocs/modules/imlinks/icms_version.php
+++ htdocs/modules/imlinks/icms_version.php
@@ -53,9 +53,12 @@ $modversion['credits'] 			= '<b>WF-Projects Team:</b> Based on WF-Links, thanks 
 $modversion['author_website_url'] 	= 'http://members.lycos.nl/mcdonaldsstore/';
 $modversion['author_website_name'] 	= 'McDonalds Store';
 
+/*
+*** COMMENTED OUT UNTIL REl="EXTERNAL" GETS FIXED ***
 $modversion['support_site_url'] = 'http://community.impresscms.org/modules/newbb/viewforum.php?forum=9';
 $modversion['support_site_name']= 'ImpressCMS Community Forum - Modules Support';
 $modversion['submit_bug'] 		= 'https://sourceforge.net/tracker2/?words=tracker_browse&sort=open_date&sortdir=desc&offset=0&group_id=205633&atid=1064496&assignee=&status=&category=1164361&artgroup=&keyword=&submitter=&artifact_id=';
+*/
 
 $modversion['warning'] 			= _MI_IMLINKS_WARNINGTEXT;
 $modversion['author_credits'] 	= _MI_IMLINKS_AUTHOR_CREDITSTEXT;
