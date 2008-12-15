f43ab5099cb6b3e85a97343f92734d6e04f226fe
Ronald <pietjebell31@hotmail.com>
2008-12-15 20:39:34 +0000

    Replaced XOOPS_ROOT_PATH with ICMS_ROOT_PATH, organising code
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imlinks/trunk/htdocs/modules/imlinks@7707 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- include/imlinks_moduleabout.php
+++ include/imlinks_moduleabout.php
@@ -1,8 +1,6 @@
 <?php
 
-if (!defined("ICMS_ROOT_PATH")) {
-die("ImpressCMS root path not defined");
-}
+if ( !defined( 'ICMS_ROOT_PATH' ) ) { die( 'ImpressCMS root path not defined' ); }
 
 /**
 * IcmsModuleAbout
@@ -17,8 +15,7 @@ die("ImpressCMS root path not defined");
 * @version		$Id: icmsmoduleabout.php 6049 2008-10-27 10:48:55Z mcdonald3072 $
 */
 
-class imLinksModuleAbout
-{
+class imLinksModuleAbout {
 	var $_lang_aboutTitle;
 	var $_lang_author_info;
 	var $_lang_developer_lead;
@@ -53,27 +50,27 @@ class imLinksModuleAbout
 	function imLinksModuleAbout( $aboutTitle='About' ) {
 		global $xoopsModule, $xoopsConfig;
 
-		icms_loadLanguageFile($xoopsModule->dirname(), 'modinfo');
-		icms_loadLanguageFile($xoopsModule->dirname(), 'moduleabout');
+		icms_loadLanguageFile( $xoopsModule -> dirname(), 'modinfo' );
+		icms_loadLanguageFile( $xoopsModule -> dirname(), 'moduleabout' );
 		
-		$this->_aboutTitle = $aboutTitle;
+		$this -> _aboutTitle = $aboutTitle;
 
-		$this->_lang_developer_contributor = _MODABOUT_IMLINKS_DEVELOPER_CONTRIBUTOR;
-		$this->_lang_developer_website = _MODABOUT_IMLINKS_DEVELOPER_WEBSITE;
-		$this->_lang_developer_email = _MODABOUT_IMLINKS_DEVELOPER_EMAIL;
-		$this->_lang_developer_credits = _MODABOUT_IMLINKS_DEVELOPER_CREDITS;
-		$this->_lang_module_info = _MODABOUT_IMLINKS_MODULE_INFO;
-		$this->_lang_module_status = _MODABOUT_IMLINKS_MODULE_STATUS;
-		$this->_lang_module_release_date =_MODABOUT_IMLINKS_MODULE_RELEASE_DATE ;
-		$this->_lang_module_demo = _MODABOUT_IMLINKS_MODULE_DEMO;
-		$this->_lang_module_support = _CO_ICMS_MODULE_SUPPORT;
-		$this->_lang_module_bug = _MODABOUT_IMLINKS_MODULE_BUG;
-		$this->_lang_module_submit_bug = _MODABOUT_IMLINKS_MODULE_SUBMIT_BUG;
-		$this->_lang_module_feature = _MODABOUT_IMLINKS_MODULE_FEATURE;
-		$this->_lang_module_submit_feature = _CO_ICMS_MODULE_SUBMIT_FEATURE;
-		$this->_lang_module_disclaimer = _MODABOUT_IMLINKS_MODULE_DISCLAIMER;
-		$this->_lang_author_word = _MODABOUT_IMLINKS_AUTHOR_WORD;
-		$this->_lang_version_history = _MODABOUT_IMLINKS_VERSION_HISTORY;
+		$this -> _lang_developer_contributor = _MODABOUT_IMLINKS_DEVELOPER_CONTRIBUTOR;
+		$this -> _lang_developer_website = _MODABOUT_IMLINKS_DEVELOPER_WEBSITE;
+		$this -> _lang_developer_email = _MODABOUT_IMLINKS_DEVELOPER_EMAIL;
+		$this -> _lang_developer_credits = _MODABOUT_IMLINKS_DEVELOPER_CREDITS;
+		$this -> _lang_module_info = _MODABOUT_IMLINKS_MODULE_INFO;
+		$this -> _lang_module_status = _MODABOUT_IMLINKS_MODULE_STATUS;
+		$this -> _lang_module_release_date =_MODABOUT_IMLINKS_MODULE_RELEASE_DATE ;
+		$this -> _lang_module_demo = _MODABOUT_IMLINKS_MODULE_DEMO;
+		$this -> _lang_module_support = _CO_ICMS_MODULE_SUPPORT;
+		$this -> _lang_module_bug = _MODABOUT_IMLINKS_MODULE_BUG;
+		$this -> _lang_module_submit_bug = _MODABOUT_IMLINKS_MODULE_SUBMIT_BUG;
+		$this -> _lang_module_feature = _MODABOUT_IMLINKS_MODULE_FEATURE;
+		$this -> _lang_module_submit_feature = _CO_ICMS_MODULE_SUBMIT_FEATURE;
+		$this -> _lang_module_disclaimer = _MODABOUT_IMLINKS_MODULE_DISCLAIMER;
+		$this -> _lang_author_word = _MODABOUT_IMLINKS_AUTHOR_WORD;
+		$this -> _lang_version_history = _MODABOUT_IMLINKS_VERSION_HISTORY;
 	}
 
 	/**
@@ -82,17 +79,16 @@ class imLinksModuleAbout
 	 * @param string $value to be sanitized
 	 * @return string santizied value
 	 */
-	function sanitize($value) {
+	function sanitize( $value ) {
 		$myts = MyTextSanitizer::getInstance();
-		return $myts->displayTarea($value, 1);
+		return $myts -> displayTarea( $value, 1 );
 	}
 
 	/**
 	 * Render the whole About page of a module
 	 *
 	 */
-	function render()
-	{
+	function render() {
 		/**
 		 * @todo make the output XHTML compliant
 		 */
@@ -103,98 +99,98 @@ class imLinksModuleAbout
 
 		xoops_cp_header();
 
-		$module_handler = &xoops_gethandler('module');
-		$versioninfo = &$module_handler->get($xoopsModule->getVar('mid'));
+		$module_handler = &xoops_gethandler( 'module' );
+		$versioninfo = &$module_handler -> get( $xoopsModule -> getVar( 'mid' ) );
 
-		$xoopsModule->displayAdminMenu(-1, $this->_aboutTitle . ' ' . $versioninfo->getInfo('name'));
+		$xoopsModule -> displayAdminMenu( -1, $this -> _aboutTitle . ' ' . $versioninfo -> getInfo( 'name' ) );
 
 		include_once ICMS_ROOT_PATH . '/class/template.php';
 
-		$this->_tpl =& new XoopsTpl();
+		$this -> _tpl =& new XoopsTpl();
 
-		$this->_tpl->assign('module_url', ICMS_URL . '/modules/' . $xoopsModule->getVar('dirname') . '/');
-		$this->_tpl->assign('module_image', $versioninfo->getInfo('image'));
-		$this->_tpl->assign('module_name', $versioninfo->getInfo('name'));
-		$this->_tpl->assign('module_version', $versioninfo->getInfo('version'));
-		$this->_tpl->assign('module_status_version', $versioninfo->getInfo('status_version'));
+		$this -> _tpl -> assign( 'module_url', ICMS_URL . '/modules/' . $xoopsModule -> getVar( 'dirname' ) . '/' );
+		$this -> _tpl -> assign( 'module_image', $versioninfo -> getInfo( 'image' ) );
+		$this -> _tpl -> assign( 'module_name', $versioninfo -> getInfo( 'name' ) );
+		$this -> _tpl -> assign( 'module_version', $versioninfo -> getInfo( 'version' ) );
+		$this -> _tpl -> assign( 'module_status_version', $versioninfo -> getInfo( 'status_version' ) );
 
 		// Left headings...
-		if ($versioninfo->getInfo('author_realname') != '') {
-			$author_name = $versioninfo->getInfo('author') . ' (' . $versioninfo->getInfo('author_realname') . ')';
+		if ( $versioninfo -> getInfo( 'author_realname' ) != '' ) {
+			$author_name = $versioninfo -> getInfo( 'author' ) . ' (' . $versioninfo -> getInfo( 'author_realname' ) . ')';
 		} else {
-			$author_name = $versioninfo->getInfo('author');
+			$author_name = $versioninfo -> getInfo( 'author' );
 		}
-		$this->_tpl->assign('module_author_name', $author_name);
+		$this -> _tpl -> assign( 'module_author_name', $author_name );
 
-		$this->_tpl->assign('module_license', $versioninfo->getInfo('license'));
+		$this -> _tpl -> assign( 'module_license', $versioninfo -> getInfo( 'license' ) );
 
-		$this->_tpl->assign('module_credits', $versioninfo->getInfo('credits'));
+		$this -> _tpl -> assign( 'module_credits', $versioninfo -> getInfo( 'credits' ) );
 
 		// Developers Information
-		$this->_tpl->assign('module_developer_lead', $versioninfo->getInfo('developer_lead'));
-		$this->_tpl->assign('module_developer_contributor', $versioninfo->getInfo('developer_contributor'));
-		$this->_tpl->assign('module_developer_website_url', $versioninfo->getInfo('developer_website_url'));
-		$this->_tpl->assign('module_developer_website_name', $versioninfo->getInfo('developer_website_name'));
-		$this->_tpl->assign('module_developer_email', $versioninfo->getInfo('developer_email'));
+		$this -> _tpl -> assign( 'module_developer_lead', $versioninfo -> getInfo( 'developer_lead' ) );
+		$this -> _tpl -> assign( 'module_developer_contributor', $versioninfo -> getInfo( 'developer_contributor' ) );
+		$this -> _tpl -> assign( 'module_developer_website_url', $versioninfo -> getInfo( 'developer_website_url' ) );
+		$this -> _tpl -> assign( 'module_developer_website_name', $versioninfo -> getInfo( 'developer_website_name' ) );
+		$this -> _tpl -> assign( 'module_developer_email', $versioninfo -> getInfo( 'developer_email' ) );
 
-		$people = $versioninfo->getInfo('people');
-		if ($people) {
+		$people = $versioninfo -> getInfo( 'people' );
+		if ( $people ) {
 
-			$this->_tpl->assign('module_people_developers', isset($people['developers']) ? array_map(array($this, 'sanitize'), $people['developers']) : false);
-			$this->_tpl->assign('module_people_testers', isset($people['testers']) ? array_map(array($this, 'sanitize'), $people['testers']) : false);
-			$this->_tpl->assign('module_people_translators', isset($people['translators']) ? array_map(array($this, 'sanitize'), $people['translators']) : false);
-			$this->_tpl->assign('module_people_documenters', isset($people['documenters']) ? array_map(array($this, 'sanitize'), $people['documenters']) : false);
-			$this->_tpl->assign('module_people_other', isset($people['other']) ? array_map(array($this, 'sanitize'), $people['other']) : false);
+			$this -> _tpl -> assign( 'module_people_developers', isset( $people['developers'] ) ? array_map( array( $this, 'sanitize' ), $people['developers'] ) : false );
+			$this -> _tpl -> assign( 'module_people_testers', isset( $people['testers'] ) ? array_map( array( $this, 'sanitize' ), $people['testers'] ) : false );
+			$this -> _tpl -> assign( 'module_people_translators', isset( $people['translators'] ) ? array_map( array( $this, 'sanitize' ), $people['translators'] ) : false );
+			$this -> _tpl -> assign( 'module_people_documenters', isset( $people['documenters'] ) ? array_map( array( $this, 'sanitize' ), $people['documenters'] ) : false );
+			$this -> _tpl -> assign( 'module_people_other', isset( $people['other'] ) ? array_map( array( $this, 'sanitize' ), $people['other'] ) : false );
 		}
 		//$this->_tpl->assign('module_developers', $versioninfo->getInfo('developer_email'));
 
 		// Module Development information
-		$this->_tpl->assign('module_date', $versioninfo->getInfo('date'));
-		$this->_tpl->assign('module_status', $versioninfo->getInfo('status'));
-		$this->_tpl->assign('module_demo_site_url', $versioninfo->getInfo('demo_site_url'));
-		$this->_tpl->assign('module_demo_site_name', $versioninfo->getInfo('demo_site_name'));
-		$this->_tpl->assign('module_support_site_url', $versioninfo->getInfo('support_site_url'));
-		$this->_tpl->assign('module_support_site_name', $versioninfo->getInfo('support_site_name'));
-		$this->_tpl->assign('module_submit_bug', $versioninfo->getInfo('submit_bug'));
-		$this->_tpl->assign('module_submit_feature', $versioninfo->getInfo('submit_feature'));
+		$this -> _tpl->assign( 'module_date', $versioninfo -> getInfo( 'date' ) );
+		$this -> _tpl->assign( 'module_status', $versioninfo -> getInfo( 'status' ) );
+		$this -> _tpl->assign( 'module_demo_site_url', $versioninfo -> getInfo( 'demo_site_url' ) );
+		$this -> _tpl->assign( 'module_demo_site_name', $versioninfo -> getInfo( 'demo_site_name' ) );
+		$this -> _tpl->assign( 'module_support_site_url', $versioninfo -> getInfo( 'support_site_url' ) );
+		$this -> _tpl->assign( 'module_support_site_name', $versioninfo -> getInfo( 'support_site_name' ) );
+		$this -> _tpl->assign( 'module_submit_bug', $versioninfo -> getInfo( 'submit_bug' ) );
+		$this -> _tpl->assign( 'module_submit_feature', $versioninfo -> getInfo( 'submit_feature' ) );
 		
 		// Manual
-		$manual =$versioninfo->getInfo('manual');
-		if ($manual) {
-			$this->_tpl->assign('module_manual', isset($manual['wiki']) ? array_map(array($this, 'sanitize'), $manual['wiki']) : false);
+		$manual =$versioninfo -> getInfo( 'manual' );
+		if ( $manual ) {
+			$this -> _tpl -> assign( 'module_manual', isset( $manual['wiki'] ) ? array_map( array( $this, 'sanitize' ), $manual['wiki']) : false );
 		}
 
 		// Warning
-		$this->_tpl->assign('module_warning', $this->sanitize($versioninfo->getInfo('warning')));
+		$this -> _tpl -> assign( 'module_warning', $this -> sanitize( $versioninfo -> getInfo( 'warning' ) ) );
 
 		// Author's note
-		$this->_tpl->assign('module_author_word', $versioninfo->getInfo('author_word'));
+		$this -> _tpl -> assign( 'module_author_word', $versioninfo -> getInfo( 'author_word' ) );
 
 	    // For changelog thanks to 3Dev
 	    //global $xoopsModule;
-	    $filename = XOOPS_ROOT_PATH . '/modules/' . $xoopsModule->getVar('dirname') . '/changelog.txt';
-	    if(is_file($filename)){
+	    $filename = ICMS_ROOT_PATH . '/modules/' . $xoopsModule -> getVar( 'dirname' ) . '/changelog.txt';
+	    if ( is_file( $filename ) ) {
 
-	        $filesize = filesize($filename);
-	        $handle = fopen($filename, 'r');
-	        $this->_tpl->assign('module_version_history', $myts->displayTarea(fread($handle, $filesize), true));
-	        fclose($handle);
+	        $filesize = filesize( $filename );
+	        $handle = fopen( $filename, 'r' );
+	        $this -> _tpl -> assign( 'module_version_history', $myts -> displayTarea( fread( $handle, $filesize ), true ) );
+	        fclose( $handle );
 	    }
 		
 		// For license thanks to 3Dev
-		if ( file_exists( XOOPS_ROOT_PATH . '/modules/' . $xoopsModule->getVar('dirname') . '/license/' . $xoopsConfig['language'] . '_license.txt' ) ) {
-			$filename = XOOPS_ROOT_PATH . '/modules/' . $xoopsModule->getVar('dirname') . '/license/' . $xoopsConfig['language'] . '_license.txt';
-		} elseif ( file_exists( XOOPS_ROOT_PATH . '/modules/' . $xoopsModule->getVar('dirname') . '/license.txt' ) ) {
-			$filename = XOOPS_ROOT_PATH . '/modules/' . $xoopsModule->getVar('dirname') . '/license.txt';
+		if ( file_exists( ICMS_ROOT_PATH . '/modules/' . $xoopsModule -> getVar( 'dirname' ) . '/license/' . $xoopsConfig['language'] . '_license.txt' ) ) {
+			$filename = ICMS_ROOT_PATH . '/modules/' . $xoopsModule -> getVar( 'dirname' ) . '/license/' . $xoopsConfig['language'] . '_license.txt';
+		} elseif ( file_exists( ICMS_ROOT_PATH . '/modules/' . $xoopsModule -> getVar( 'dirname' ) . '/license.txt' ) ) {
+			$filename = ICMS_ROOT_PATH . '/modules/' . $xoopsModule -> getVar( 'dirname' ) . '/license.txt';
 		}
-	    if(is_file($filename)){
-	        $filesize = filesize($filename);
-	        $handle = fopen($filename, 'r');
-	        $this->_tpl->assign('module_license_txt', $myts->displayTarea(fread($handle, $filesize), 0, 0, 1, 1, 1, true));
+	    if( is_file( $filename ) ) {
+	        $filesize = filesize( $filename );
+	        $handle = fopen( $filename, 'r' );
+	        $this -> _tpl -> assign( 'module_license_txt', $myts -> displayTarea( fread( $handle, $filesize ), 0, 0, 1, 1, 1, true ) );
 	        fclose($handle);
 	    }
 
-		$this->_tpl->display(XOOPS_ROOT_PATH . '/modules/' . $xoopsModule->getVar('dirname') . '/templates/imlinks_moduleabout.html');
+		$this -> _tpl -> display( ICMS_ROOT_PATH . '/modules/' . $xoopsModule -> getVar( 'dirname' ) . '/templates/imlinks_moduleabout.html' );
 
 		xoops_cp_footer();
 	}
