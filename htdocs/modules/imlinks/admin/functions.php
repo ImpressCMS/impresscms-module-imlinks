afaba20baa98b8720c84bede72ded80f0e6bfa4b
Ronald <pietjebell31@hotmail.com>
2011-11-10 19:29:39 +0000

    Status of submitted links will be indicated with an hourglass icon
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imlinks/trunk@22978 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- htdocs/modules/imlinks/admin/functions.php
+++ htdocs/modules/imlinks/admin/functions.php
@@ -182,11 +182,17 @@ function iml_linklistbody( $published ) {
     $publish = ( $published['published'] > 0 ) ? formatTimestamp( $published['published'], icms::$module -> config['dateformatadmin'] ): 'Not Published';
     $expires = $published['expired'] ? formatTimestamp( $published['expired'], icms::$module -> config['dateformatadmin'] ): _AM_IMLINKS_MINDEX_NOTSET;
 	
-    if ( ( ( $published['expired'] && $published['expired'] > time() ) OR  $published['expired']==0)&& ( $published['published'] && $published['published'] < time() ) && $published['offline'] == 0 ) {
+	if ( $published['status'] == 0 && $published['offline'] == 0 ) {
+		// Link submitted, waiting for approval
+		$published_status = $imagearray['waiting'];
+    } elseif ( ( ( $published['expired'] && $published['expired'] > time() ) OR  $published['expired']==0)&& ( $published['published'] && $published['published'] < time() ) && $published['offline'] == 0 ) {
+		// Online
         $published_status = '<a href="index.php?op=status_off&amp;lid=' . $lid . '">' . $imagearray['online'] . '</a>';
     } elseif ( ( $published['expired'] && $published['expired'] < time() )  && $published['offline'] == 0 ) {
+		// Expired
         $published_status = $imagearray['expired'];
     } else {
+		// Offline
         $published_status = ( $published['published'] == 0 ) ? "<a href='newlinks.php'>" . $imagearray['offline'] . "</a>" : '<a href="index.php?op=status_on&amp;lid=' . $lid . '">' . $imagearray['offline'] . '</a>';
     }
 	
