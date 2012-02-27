54fcb8f677f56f6101e8f8cfc073bbf8f993306b
Ronald <pietjebell31@hotmail.com>
2012-02-27 17:33:12 +0000

    Fixed: MySQL errors when updating
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imlinks/trunk@23753 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- htdocs/modules/imlinks/include/onupdate.inc.php
+++ htdocs/modules/imlinks/include/onupdate.inc.php
@@ -33,125 +33,70 @@ $ref = xoops_getenv( 'HTTP_REFERER' );
 
 if( $ref == '' || strpos( $ref , ICMS_URL . '/modules/system/admin.php' ) === 0 ) {
 
-	$i=0;
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_broken') . " MODIFY confirmed TINYINT(1) NOT NULL default '0'" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_broken') . " MODIFY acknowledged TINYINT(1) NOT NULL default '0'" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_broken') . " MODIFY confirmed TINYINT(1) NOT NULL default '0'";
+	if ( !mysql_query( $sql ) ) {}
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_broken') . " MODIFY acknowledged TINYINT(1) NOT NULL default '0'";
+	if ( !mysql_query( $sql ) ) {}
 
 	// imLinks 1.03
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_links') . " ADD COLUMN nice_url VARCHAR(100) NOT NULL default ''" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_links') . " ADD COLUMN ttlat VARCHAR(100) NOT NULL default ''" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_links') . " ADD COLUMN ttlong VARCHAR(100) NOT NULL default ''" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_mod') . " ADD COLUMN ttlat VARCHAR(100) NOT NULL default ''" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_mod') . " ADD COLUMN ttlong VARCHAR(100) NOT NULL default ''" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_links') . " ADD COLUMN nice_url VARCHAR(100) NOT NULL default ''";
+	if ( !mysql_query( $sql ) ) {}
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_links') . " ADD COLUMN ttlat VARCHAR(100) NOT NULL default ''";
+	if ( !mysql_query( $sql ) ) {}
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_links') . " ADD COLUMN ttlong VARCHAR(100) NOT NULL default ''";
+	if ( !mysql_query( $sql ) ) {}
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_mod') . " ADD COLUMN ttlat VARCHAR(100) NOT NULL default ''";
+	if ( !mysql_query( $sql ) ) {}
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_mod') . " ADD COLUMN ttlong VARCHAR(100) NOT NULL default ''";
+	if ( !mysql_query( $sql ) ) {}
 
 	// imLinks 1.05
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_links') . " DROP urlrating" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_links') . " DROP nobreak" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_links') . " DROP urlrating";
+	if ( !mysql_query( $sql ) ) {}
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_links') . " DROP nobreak";
+	if ( !mysql_query( $sql ) ) {}
 
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_mod') . " DROP urlrating" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_mod') . " DROP nobreak" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_mod') . " DROP urlrating";
+	if ( !mysql_query( $sql ) ) {}
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_mod') . " DROP nobreak";
+	if ( !mysql_query( $sql ) ) {}
 
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_indexpage') . " DROP nohtml" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_indexpage') . " DROP nosmiley" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_indexpage') . " DROP noxcodes" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_indexpage') . " DROP noimages" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_indexpage') . " DROP nobreak" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_indexpage') . " DROP nohtml";
+	if ( !mysql_query( $sql ) ) {}
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_indexpage') . " DROP nosmiley";
+	if ( !mysql_query( $sql ) ) {}
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_indexpage') . " DROP noxcodes";
+	if ( !mysql_query( $sql ) ) {}
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_indexpage') . " DROP noimages";
+	if ( !mysql_query( $sql ) ) {}
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_indexpage') . " DROP nobreak";
+	if ( !mysql_query( $sql ) ) {}
 
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_cat') . " DROP nohtml" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_cat') . " DROP nosmiley" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_cat') . " DROP noxcodes" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_cat') . " DROP noimages" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_cat') . " DROP nobreak" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_cat') . " DROP spotlighttop" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_cat') . " DROP spotlighthis" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_cat') . " DROP total" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_cat') . " DROP nohtml";
+	if ( !mysql_query( $sql ) ) {}
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_cat') . " DROP nosmiley";
+	if ( !mysql_query( $sql ) ) {}
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_cat') . " DROP noxcodes";
+	if ( !mysql_query( $sql ) ) {}
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_cat') . " DROP noimages";
+	if ( !mysql_query( $sql ) ) {}
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_cat') . " DROP nobreak";
+	if ( !mysql_query( $sql ) ) {}
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_cat') . " DROP spotlighttop";
+	if ( !mysql_query( $sql ) ) {}
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_cat') . " DROP spotlighthis";
+	if ( !mysql_query( $sql ) ) {}
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_cat') . " DROP total";
+	if ( !mysql_query( $sql ) ) {}
 
 	// imLinks 1.11
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_cat') . " DROP client_id" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_cat') . " DROP banner_id" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_cat') . " DROP client_id";
+	if ( !mysql_query( $sql ) ) {}
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_cat') . " DROP banner_id";
+	if ( !mysql_query( $sql ) ) {}
 
 	// imLinks 1.12
-	$i++;
-	$ret[$i] = true;
-	$query[$i] = sprintf( "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_indexpage') . " DROP lastlinksyn" );
-	$ret[$i] = $ret[$i] && icms::$xoopsDB -> query( $query[$i] );
+	$sql = "ALTER TABLE " . icms::$xoopsDB -> prefix( 'imlinks_indexpage') . " DROP lastlinksyn";
+	if ( !mysql_query( $sql ) ) {}
 }
 ?>
