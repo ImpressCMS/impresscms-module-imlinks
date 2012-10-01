20a917edcc66d8b24c21b5bf69193dbe239bedd4
Ronald <pietjebell31@hotmail.com>
2012-10-01 17:17:28 +0000

    Task: ipf version of imLinks
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imlinks/tasks/imlinks_ipf@24489 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ language/czech/blocks.php
@@ -0,0 +1,56 @@
+<?php
+/**
+* imLinks - a multicategory links management module for ImpressCMS
+*
+* Based upon WF-Links 1.06
+*
+* File: language/czech/blocks.php
+*
+* @copyright	http://www.xoops.org/ The XOOPS Project
+* @copyright	XOOPS_copyrights.txt
+* @copyright	http://www.impresscms.org/ The ImpressCMS Project
+* @license		http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License (GPL)
+* ----------------------------------------------------------------------------------------------------------
+* @package		WF-Links 
+* @since		1.03
+* @author		John N
+* ----------------------------------------------------------------------------------------------------------
+* @package		WF-Links 
+* @since		1.03b and 1.03c
+* @author		McDonald
+* ----------------------------------------------------------------------------------------------------------
+* @package		imLinks
+* @since		1.00
+* @author		McDonald
+* @version		$Id$
+*/
+
+// Blocks
+define( '_MB_IMLINKS_DISP', 'Zobrazit' );
+define( '_MB_IMLINKS_FILES', 'odkazů' );
+define( '_MB_IMLINKS_CHARS', 'Délka názvu' );
+define( '_MB_IMLINKS_LENGTH', 'znaků' );
+
+// Version 1.05 RC5
+define( '_MB_IMLINKS_ID', 'ID' );
+define( '_MB_IMLINKS_CLIENT', 'Jméno klienta' );
+define( '_MB_IMLINKS_BANNERID', 'ID banneru' );
+define( '_MB_IMLINKS_IMPMADE', 'Impr. made' );
+define( '_MB_IMLINKS_IMPLEFT', 'Impr. left' );
+define( '_MB_IMLINKS_CLICKS', 'Clicks' );
+define( '_MB_IMLINKS_CATTITLE', 'Kategorie' );
+define( '_MB_IMLINKS_UNLIMIT', 'neomezeně' );
+define( '_MB_IMLINKS_DATEFORMAT', 'Formát datumu' );
+define( '_MB_IMLINKS_DATEFORMATMANUAL', "Podívejte se do <a href='http://jp.php.net/manual/en/function.date.php' target='_blank'>PHP manuálu</a>" );
+
+// Version 1.12
+define( '_MB_IMLINKS_SUBMITTER', 'Submitter' );
+define( '_MB_IMLINKS_PUBLISHER', 'Publisher' );
+define( '_MB_IMLINKS_DATE', 'Date' );
+define( '_MB_IMLINKS_COUNTRY', 'Country' );
+define( '_MB_IMLINKS_HITS', 'Hits' );
+define( '_MB_IMLINKS_NOTSPECIFIED', 'Not specified' );
+define( '_MB_IMLINKS_VIEWDETAILS', 'View Full Details' );
+define( '_MB_IMLINKS_ADMINSECTION', 'Administration' );
+define( '_MB_IMLINKS_SCRSHOTENLARGE', 'Click image to enlarge' );
+?>
