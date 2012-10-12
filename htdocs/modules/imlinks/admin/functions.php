eee2a049514f6abb138e1cb99c5ac662edf0cf1b
Ronald <pietjebell31@hotmail.com>
2012-10-12 19:30:11 +0000

    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/imlinks/trunk@24580 7a4d4d67-0c3a-0410-b24c-89d50c169223

---
+++ htdocs/modules/imlinks/admin/functions.php
@@ -0,0 +1,204 @@
+<?php
+/**
+* imLinks - a multicategory links management module for ImpressCMS
+*
+* Based upon WF-Links 1.06
+*
+* File: admin/functions.php
+*
+* @copyright	http://www.xoops.org/ The XOOPS Project
+* @copyright	XOOPS_copyrights.txt
+* @copyright	http://www.impresscms.org/ The ImpressCMS Project
+* @license		GNU General Public License (GPL)
+*				a copy of the GNU license is enclosed.
+* ----------------------------------------------------------------------------------------------------------
+* @package		WF-Links 
+* @since		1.03
+* @author		John N
+* ----------------------------------------------------------------------------------------------------------
+* 				WF-Links 
+* @since		1.03b and 1.03c
+* @author		McDonald
+* ----------------------------------------------------------------------------------------------------------
+* 				imLinks
+* @since		1.00
+* @author		McDonald
+* @version		$Id$
+*/
+
+
+// iml_serverstats()
+// @return
+function iml_serverstats() {
+	echo '<div style="border: #e8e8e8 1px solid; padding: 8px; border-radius: 5px;">
+			<div style="display: inline; font-weight: bold; color: #0A3760;">' . _AM_IMLINKS_LINK_IMAGEINFO . '</div>
+			<img src="' . ICMS_URL . '/modules/' . icms::$module -> getVar( 'dirname' ) . '/images/icon/server.png" alt="" style="float: left; padding-right: 10px;" />
+			<div>' . _AM_IMLINKS_LINK_SPHPINI . '</div>';
+	$safemode = ( ini_get( 'safe_mode' ) ) ? _AM_IMLINKS_LINK_ON . _AM_IMLINKS_LINK_SAFEMODEPROBLEMS : _AM_IMLINKS_LINK_OFF;
+	$registerglobals = ( ini_get( 'register_globals' ) == '' ) ? _AM_IMLINKS_LINK_OFF : _AM_IMLINKS_LINK_ON;
+	$links = ( ini_get( 'file_uploads' ) ) ? _AM_IMLINKS_LINK_ON : _AM_IMLINKS_LINK_OFF;
+	$gdlib = ( function_exists( 'gd_info' ) ) ? _AM_IMLINKS_LINK_GDON : _AM_IMLINKS_LINK_GDOFF;
+	echo '<div style="padding-left: 44px;">&bull;&nbsp;' . _AM_IMLINKS_LINK_GDLIBSTATUS . $gdlib . '<br />';
+	if ( function_exists( 'gd_info' ) ) {
+		if ( true == $gdlib = gd_info() ) {
+			echo '&bull;&nbsp;' . _AM_IMLINKS_LINK_GDLIBVERSION . '<b>' . $gdlib['GD Version'] . '</b>';
+		}
+	}
+	echo '<br /><br />';
+	echo '&bull;&nbsp;' . _AM_IMLINKS_LINK_SAFEMODESTATUS . $safemode . '<br />';
+	echo '&bull;&nbsp;' . _AM_IMLINKS_LINK_REGISTERGLOBALS . $registerglobals . '<br />';
+	echo '&bull;&nbsp;' . _AM_IMLINKS_LINK_SERVERUPLOADSTATUS . $links;
+	echo '</div></div><br />';
+}
+
+function iml_adminmenu( $currentoption = 0, $header = '', $menu = '', $extra = '', $scount = 4 ) {
+	icms::$module -> displayAdminMenu( $currentoption, icms::$module -> getVar( 'name' ) . ' | ' . $header );
+
+	// ###### Output warn messages for security ######
+	if ( is_dir( ICMS_ROOT_PATH . '/modules/' . icms::$module -> getVar( 'dirname' ) . '/update/' ) ) {
+		icms_core_Message::error( sprintf( _AM_IMLINKS_WARNINSTALL1, ICMS_ROOT_PATH . '/modules/' . icms::$module -> getVar( 'dirname' ) . '/update/' ) );
+	}
+
+	$_file = ICMS_ROOT_PATH . '/modules/' . icms::$module -> getVar( 'dirname' ) . '/update.php';
+	if ( file_exists( $_file ) ) {
+		icms_core_Message::error( sprintf( _AM_IMLINKS_WARNINSTALL2, ICMS_ROOT_PATH . '/modules/' . icms::$module -> getVar( 'dirname' ) . '/update.php' ) );
+	}
+
+	$path1 = ICMS_ROOT_PATH . '/' . icms::$module -> config['mainimagedir'];
+	if ( !is_dir( $path1 ) ) { icms_core_Message::error( sprintf( _AM_IMLINKS_WARNINSTALL3, $path1 ) ); }
+	if ( !is_writable( $path1 ) ) { icms_core_Message::error( sprintf( _AM_IMLINKS_WARNINSTALL4, $path1 ) ); }
+
+	$path1_t = ICMS_ROOT_PATH . '/' . icms::$module -> config['mainimagedir'] . '/thumbs';
+	if ( !is_dir( $path1_t ) ) { icms_core_Message::error( sprintf( _AM_IMLINKS_WARNINSTALL3, $path1_t ) ); }
+	if ( !is_writable( $path1_t ) ) { icms_core_Message::error( sprintf( _AM_IMLINKS_WARNINSTALL4, $path1_t ) ); }
+
+	$path2 = ICMS_ROOT_PATH . '/' . icms::$module -> config['screenshots'];
+	if ( !is_dir( $path2 ) ) { icms_core_Message::error( sprintf( _AM_IMLINKS_WARNINSTALL3, $path2 ) ); }
+	if ( !is_writable( $path2 ) ) { icms_core_Message::error( sprintf( _AM_IMLINKS_WARNINSTALL4, $path2 ) ); }
+
+	$path2_t = ICMS_ROOT_PATH . '/' . icms::$module -> config['screenshots'] . '/thumbs';
+	if ( !is_dir( $path2_t ) ) { icms_core_Message::error( sprintf( _AM_IMLINKS_WARNINSTALL3, $path2_t ) ); }
+	if ( !is_writable( $path2_t ) ) { icms_core_Message::error( sprintf( _AM_IMLINKS_WARNINSTALL4, $path2_t ) ); }
+
+	$path3 = ICMS_ROOT_PATH . '/' . icms::$module -> config['catimage'];
+	if ( !is_dir( $path3 ) ) { icms_core_Message::error( sprintf( _AM_IMLINKS_WARNINSTALL3, $path3 ) ); }
+	if ( !is_writable( $path3 ) ) { icms_core_Message::error( sprintf( _AM_IMLINKS_WARNINSTALL4, $path3 ) ); }
+
+	$path3_t = ICMS_ROOT_PATH . '/' . icms::$module -> config['catimage'] . '/thumbs';
+	if ( !is_dir( $path3_t ) ) { icms_core_Message::error( sprintf( _AM_IMLINKS_WARNINSTALL3, $path3_t ) ); }
+	if ( !is_writable( $path3_t ) ) { icms_core_Message::error( sprintf( _AM_IMLINKS_WARNINSTALL4, $path3_t ) ); }
+
+	// Check if library folders are present
+	$path_tickers_01 = ICMS_LIBRARIES_PATH . '/greybox';
+	$path_tickers_02 = ICMS_LIBRARIES_PATH . '/lightwindow';
+	$path_tickers_03 = ICMS_LIBRARIES_PATH . '/lytebox';
+	$path_tickers_04 = ICMS_LIBRARIES_PATH . '/prototype';
+	$path_tickers_05 = ICMS_LIBRARIES_PATH . '/scriptaculous';
+	$path_tickers_06 = ICMS_LIBRARIES_PATH . '/socialshareprivacy';
+	if ( !is_dir( $path_tickers_01 ) ) { icms_core_Message::error( sprintf( _AM_IMLINKS_WARNINSTALL3, $path_tickers_01 ) ); }
+	if ( !is_dir( $path_tickers_02 ) ) { icms_core_Message::error( sprintf( _AM_IMLINKS_WARNINSTALL3, $path_tickers_02 ) ); }
+	if ( !is_dir( $path_tickers_03 ) ) { icms_core_Message::error( sprintf( _AM_IMLINKS_WARNINSTALL3, $path_tickers_03 ) ); }
+	if ( !is_dir( $path_tickers_04 ) ) { icms_core_Message::error( sprintf( _AM_IMLINKS_WARNINSTALL3, $path_tickers_04 ) ); }
+	if ( !is_dir( $path_tickers_05 ) ) { icms_core_Message::error( sprintf( _AM_IMLINKS_WARNINSTALL3, $path_tickers_05 ) ); }
+	if ( !is_dir( $path_tickers_06 ) ) { icms_core_Message::error( sprintf( _AM_IMLINKS_WARNINSTALL3, $path_tickers_06 ) ); }
+
+	echo '<h3 style="color: #2F5376;">' . $header . '</h3>';
+	if ( $extra ) { echo '<div>$extra</div>'; }
+}
+
+function iml_linklistpagenav( $pubrowamount, $start, $art = 'art', $_this = '', $align = 'left' ) {
+	if ( ( $pubrowamount < icms::$module -> config['admin_perpage'] ) ) { return false; }
+	$pagenav = new icms_view_PageNav( $pubrowamount, icms::$module -> config['admin_perpage'], $start, 'st' . $art, $_this );
+	echo '<div style="float: ' . $align . '; font-size: .9em;">' . $pagenav -> renderNav() . '</div>';
+}
+
+function iml_uploading( $_FILES, $uploaddir = 'uploads', $allowed_mimetypes = '', $redirecturl = 'links.php', $num = 0, $redirect = 0, $usertype = 1 ) {
+	global $_FILES;
+	$down = array();
+	include_once ICMS_ROOT_PATH . '/modules/' . icms::$module -> getVar( 'dirname' ) . '/class/uploader.php';
+	if ( empty( $allowed_mimetypes ) ) {
+		$allowed_mimetypes = iml_retmime( $_FILES['userfile']['name'], $usertype );
+	}
+	$upload_dir = ICMS_ROOT_PATH . '/' . $uploaddir . '/';
+	$maxfilesize = icms::$module -> config['maxfilesize'];
+	$maxfilewidth = icms::$module -> config['maximgwidth'];
+	$maxfileheight = icms::$module -> config['maximgheight'];
+	$uploader = new imLinksMediaUploader( $upload_dir, $allowed_mimetypes, $maxfilesize, $maxfilewidth, $maxfileheight );
+	$uploader -> noAdminSizeCheck( 1 );
+	if ( $uploader -> fetchMedia( $_POST['xoops_upload_file'][0] ) ) {
+		if ( !$uploader -> upload() ) {
+			$errors = $uploader -> getErrors();
+			redirect_header( $redirecturl, 2, $errors );
+		} else {
+			if ( $redirect ) {
+				redirect_header( $redirecturl, 1 , _AM_PDD_UPLOADFILE );
+			} else {
+				if ( is_file( $uploader -> savedDestination ) ) {
+					$down['url']  = ICMS_URL . '/' . $uploaddir . '/' . strtolower( $uploader -> savedFileName );
+					$down['size'] = filesize( ICMS_ROOT_PATH . '/' . $uploaddir . '/' . strtolower( $uploader -> savedFileName ) );
+				}
+				return $down;
+			}
+		}
+	} else {
+		$errors = $uploader -> getErrors();
+		redirect_header( $redirecturl, 1, $errors );
+	}
+}
+
+function iml_cattitle( $catt ) {
+	$sql = 'SELECT title FROM ' . icms::$xoopsDB -> prefix( 'imlinks_cat' ) . ' WHERE cid=' . $catt;
+	$result = icms::$xoopsDB -> fetchArray( icms::$xoopsDB -> query( $sql ) );
+	return $result['title'];
+}
+
+function iml_linklistbody( $published ) {
+	global $imagearray;
+	$lid = $published['lid'];
+	$cid = $published['cid'];
+	$nice_link = iml_nicelink( $published['title'], $published['nice_url'] );
+	if ( icms::$module -> config['niceurl'] ) {
+		$title = '<a href="../singlelink.php?lid=' . $lid . '&amp;page=' . $nice_link . '">' . icms_core_DataFilter::htmlSpecialChars( icms_core_DataFilter::stripSlashesGPC( trim( $published['title'] ) ) ) . '</a>';
+	} else {
+		$title = '<a href="../singlelink.php?lid=' . $lid . '">' . icms_core_DataFilter::htmlSpecialChars( icms_core_DataFilter::stripSlashesGPC( trim( $published['title'] ) ) ) . '</a>';
+	}
+	$maintitle = urlencode( icms_core_DataFilter::htmlSpecialChars( icms_core_DataFilter::stripSlashesGPC( trim( $published['title'] ) ) ) );
+	$cattitle = '<a href="../viewcat.php?cid=' . $cid . '">' . iml_cattitle( $cid ) . '</a>';
+	$submitter = icms_member_user_Handler::getUserLink( $published['submitter'] );
+	$hwhoisurl = str_replace( 'http://', '', $published['url']);
+	$submitted = formatTimestamp( $published['date'], icms::$module -> config['dateformat'] );
+	$publish = ( $published['published'] > 0 ) ? formatTimestamp( $published['published'], icms::$module -> config['dateformatadmin'] ): 'Not Published';
+	$expires = $published['expired'] ? formatTimestamp( $published['expired'], icms::$module -> config['dateformatadmin'] ): _AM_IMLINKS_MINDEX_NOTSET;
+	
+	if ( $published['status'] == 0 && $published['offline'] == 0 ) {
+		// Link submitted, waiting for approval
+		$published_status = $imagearray['waiting'];
+	} elseif ( ( ( $published['expired'] && $published['expired'] > time() ) OR  $published['expired']==0)&& ( $published['published'] && $published['published'] < time() ) && $published['offline'] == 0 ) {
+		// Online
+		$published_status = '<a href="index.php?op=status_off&amp;lid=' . $lid . '">' . $imagearray['online'] . '</a>';
+	} elseif ( ( $published['expired'] && $published['expired'] < time() )  && $published['offline'] == 0 ) {
+		// Expired
+		$published_status = $imagearray['expired'];
+	} else {
+		// Offline
+		$published_status = ( $published['published'] == 0 ) ? "<a href='newlinks.php'>" . $imagearray['offline'] . "</a>" : '<a href="index.php?op=status_on&amp;lid=' . $lid . '">' . $imagearray['offline'] . '</a>';
+	}
+
+	$icon = '<a href="index.php?op=edit&amp;lid=' . $lid . '" title="' . _AM_IMLINKS_ICO_EDIT . '">' . $imagearray['editimg'] . '</a>';
+	$icon .= '<a style="padding-left: 5px;" href="index.php?op=delete&amp;lid=' . $lid . '" title="' . _AM_IMLINKS_ICO_DELETE . '">' . $imagearray['deleteimg'] . '</a>';
+	$icon .= '<a style="padding-left: 5px;" href="index.php?op=clone&amp;lid=' . $lid . '" title="' . _AM_IMLINKS_ICO_CLONE . '">' . $imagearray['clone'] . '</a>';
+	$icon .= '<a style="padding-left: 5px;" href="altcat.php?op=main&amp;lid=' . $lid . '" title="' . _AM_IMLINKS_ALTCAT_CREATEF . '">' . $imagearray['altcat'] . '</a>';
+	$icon .= '<a style="padding-left: 5px;" href="http://whois.domaintools.com/' . $hwhoisurl . '" target="_blank"><img src="' . ICMS_URL . '/modules/' . icms::$module -> getVar( 'dirname' ) . '/images/icon/domaintools.png" alt="WHOIS" title="WHOIS" align="absmiddle"/></a>';
+	echo '<div class="imlinks_tblrow">
+			<div class="imlinks_tblhdrcell" style="text-align: center;">' . $lid . '</div>
+			<div class="imlinks_tblcell">' . $title . '</div>
+			<div class="imlinks_tblcell">' . $cattitle . '</div>
+			<div class="imlinks_tblcell" style="text-align: center;">' . $submitter . '</div>
+			<div class="imlinks_tblcell" style="text-align: center;">' . $publish . '</div>
+			<div class="imlinks_tblcell" style="text-align: center;">' . $expires . '</div>
+			<div class="imlinks_tblcell" style="text-align: center;">' . $published_status . '</div>
+			<div class="imlinks_tblcell" style="white-space: nowrap; text-align: center; width: 110px;">' . $icon . '</div>
+		  </div>';
+	unset( $published );
+}
+?>
---
+++ htdocs/modules/imlinks/admin/newlinks.php
@@ -0,0 +1,167 @@
+<?php
+/**
+* imLinks - a multicategory links management module for ImpressCMS
+*
+* Based upon WF-Links 1.06
+*
+* File: admin/newlinks.php
+*
+* @copyright	http://www.xoops.org/ The XOOPS Project
+* @copyright	XOOPS_copyrights.txt
+* @copyright	http://www.impresscms.org/ The ImpressCMS Project
+* @license		GNU General Public License (GPL)
+*				a copy of the GNU license is enclosed.
+* ----------------------------------------------------------------------------------------------------------
+* @package		WF-Links 
+* @since		1.03
+* @author		John N
+* ----------------------------------------------------------------------------------------------------------
+* 				WF-Links 
+* @since		1.03b and 1.03c
+* @author		McDonald
+* ----------------------------------------------------------------------------------------------------------
+* 				imLinks
+* @since		1.00
+* @author		McDonald
+* @version		$Id$
+*/
+
+include 'admin_header.php';
+
+$op = iml_cleanRequestVars( $_REQUEST, 'op', '' );
+$lid = intval( iml_cleanRequestVars( $_REQUEST, 'lid', '' ) );
+
+$imlinks_newlinks_handler = icms_getModuleHandler( 'links', basename( dirname( dirname( __FILE__ ) ) ), 'imlinks' );
+
+switch ( strtolower( $op ) ) {
+	case 'approve':
+
+		$sql = 'SELECT cid, title, notifypub FROM ' . icms::$xoopsDB -> prefix( 'imlinks_links' ) . ' WHERE lid=' . $lid;
+		if ( !$result = icms::$xoopsDB -> query( $sql ) ) {
+			icms::$logger -> handleError( E_USER_WARNING, $sql, __FILE__, __LINE__ );
+			return false;
+		}
+		list( $cid, $title, $notifypub ) = icms::$xoopsDB -> fetchRow( $result );
+
+		// Update the database
+		$publisher = icms::$user -> getVar( 'uname' );
+		$sql2 = "UPDATE " . icms::$xoopsDB -> prefix( 'imlinks_links' ) . " SET published=" . time() . ", status='1', publisher='$publisher' WHERE lid=" . $lid;
+		if ( !$result = icms::$xoopsDB -> queryF( $sql2 ) ) {
+			icms::$logger -> handleError( E_USER_WARNING, $sql, __FILE__, __LINE__ );
+			return false;
+		}
+
+		$tags = array();
+		$tags['LINK_NAME'] = $title;
+		$tags['LINK_URL'] = ICMS_URL . '/modules/' . icms::$module -> getVar( 'dirname' ) . '/singlelink.php?lid=' . $lid;
+
+		$sql3 = 'SELECT title FROM ' . icms::$xoopsDB -> prefix( 'imlinks_cat' ) . ' WHERE cid=' . $cid;
+		if ( !$result = icms::$xoopsDB -> query( $sql3 ) ) {
+			icms::$logger -> handleError( E_USER_WARNING, $sql, __FILE__, __LINE__ );
+		} else {
+			$row = icms::$xoopsDB -> fetchArray( $result );
+			$tags['CATEGORY_NAME'] = $row['title'];
+			$tags['CATEGORY_URL'] = ICMS_URL . '/modules/' . icms::$module -> getVar( 'dirname' ) . '/viewcat.php?cid=' . $cid;
+			$notification_handler = icms::handler( 'icms_data_notification' );
+			$notification_handler -> triggerEvent( 'global', 0, 'new_link', $tags );
+			$notification_handler -> triggerEvent( 'category', $cid, 'new_link', $tags );
+			if ( $notifypub == 1 ) {
+				$notification_handler -> triggerEvent( 'link', $lid, 'approve', $tags );
+			}
+		}
+		redirect_header( 'newlinks.php', 1, _AM_IMLINKS_SUB_NEWFILECREATED );
+		break;
+
+	case 'main':
+	default:
+
+		$start = iml_cleanRequestVars( $_REQUEST, 'start', 0 );
+		$sql = 'SELECT * FROM ' . icms::$xoopsDB -> prefix( 'imlinks_links' ) . ' WHERE published=0 ORDER BY lid DESC' ;
+		if ( !$result = icms::$xoopsDB -> query( $sql ) ) {
+			icms::$logger -> handleError( E_USER_WARNING, $sql, __FILE__, __LINE__ );
+			return false;
+		}
+		$new_array = icms::$xoopsDB -> query( $sql, icms::$module -> config['admin_perpage'], $start );
+		$new_array_count = icms::$xoopsDB -> getRowsNum( icms::$xoopsDB -> query( $sql ) );
+
+		icms_cp_header();
+		iml_adminmenu( '', _AM_IMLINKS_SUB_SUBMITTEDFILES );
+
+		echo '<div style="border: #e8e8e8 1px solid; padding: 8px; border-radius: 5px;">
+				<div style="display: inline; font-weight: bold; color: #0A3760; font-size: 12px;">' . _AM_IMLINKS_SUB_FILESWAITINGINFO . '</div>
+				<div style="padding: 8px;">' . _AM_IMLINKS_SUB_FILESWAITINGVALIDATION . '<b>' . $new_array_count . '</b></div>
+				<div style="margin-left: 12px;">
+					' . $imagearray['approve'] . '&nbsp;&nbsp;' . _AM_IMLINKS_SUB_APPROVEWAITINGFILE . '<br />
+					' . $imagearray['editimg'] . '&nbsp;&nbsp;' . _AM_IMLINKS_SUB_EDITWAITINGFILE . '<br />
+					' . $imagearray['deleteimg'] . '&nbsp;&nbsp;' . _AM_IMLINKS_SUB_DELETEWAITINGFILE . '</div>
+				</div><br />';
+		echo '<link rel="stylesheet" type="text/css" href="' . ICMS_URL . '/modules/' . icms::$module -> getVar( 'dirname' ) . '/style.css" />';
+		
+		if ( icms::$module -> config['ipftables'] == 1 ) {
+		
+			$criteria = new icms_db_criteria_Compo();
+			$criteria -> add( new icms_db_criteria_Item( 'published', 0 ) );
+
+			$objectTable = new icms_ipf_view_Table( $imlinks_newlinks_handler, $criteria, array() );
+
+			$objectTable -> addColumn( new icms_ipf_view_Column( 'lid', 'center', 40, true ) );
+			$objectTable -> addColumn( new icms_ipf_view_Column( 'title', _GLOBAL_LEFT, false, 'ViewLink' ) );
+			$objectTable -> addColumn( new icms_ipf_view_Column( 'submitter', 'center', false ) );
+			$objectTable -> addColumn( new icms_ipf_view_Column( 'date', 'center' ) );
+			
+			$objectTable -> addCustomAction( 'getApprove' );
+			$objectTable -> addCustomAction( 'getEditLink' );
+			$objectTable -> addCustomAction( 'getDeleteLink' );
+		
+			$objectTable -> addQuickSearch( array( 'title' ), _AM_IMLINKS_SEARCHTITLE );
+		
+			$objectTable -> setDefaultSort( 'lid' );
+			$objectTable -> setDefaultOrder( 'DESC' );
+
+			$icmsAdminTpl -> assign( 'imlinks_newlinks_table', $objectTable -> fetch() );
+			$icmsAdminTpl -> display( 'db:imlinks_admin_index.html' );
+		
+		} else {
+			if ( $new_array_count > 0 ) {
+				echo '<div class="imlinks_table" style="font-size: 10px;">
+						<div class="imlinks_tblhdrrow">
+						<div class="imlinks_tblcell" style="text-align: center;">' . _AM_IMLINKS_MINDEX_ID . '</div>
+						<div class="imlinks_tblcell">' . _AM_IMLINKS_MINDEX_TITLE . '</div>
+						<div class="imlinks_tblcell" style="text-align: center;">' . _AM_IMLINKS_MINDEX_POSTER . '</div>
+						<div class="imlinks_tblcell" style="text-align: center;">' . _AM_IMLINKS_MINDEX_SUBMITTED . '</div>
+						<div class="imlinks_tblcell" style="text-align: center; width: 60px;">' . _AM_IMLINKS_MINDEX_ACTION . '</div>
+					</div>';
+				while ( $new = icms::$xoopsDB -> fetchArray( $new_array ) ) {
+					$lid = intval( $new['lid'] );
+					$rating = number_format( $new['rating'], 2 );
+					$title = icms_core_DataFilter::htmlSpecialChars( icms_core_DataFilter::stripSlashesGPC( $new['title'] ) );
+					$url = urldecode( icms_core_DataFilter::htmlSpecialChars( icms_core_DataFilter::stripSlashesGPC( $new['url'] ) ) );
+					$logourl = icms_core_DataFilter::htmlSpecialChars( icms_core_DataFilter::stripSlashesGPC( $new['screenshot'] ) );
+					$submitter = icms_member_user_Handler::getUserLink( $new['submitter'] );
+					$datetime = formatTimestamp( $new['date'], icms::$module -> config['dateformatadmin'] );
+
+					$icon = ( $new['published'] ) ? $approved : '<a href="newlinks.php?op=approve&amp;lid=' . $lid . '">' . $imagearray['approve'] . '</a>';
+					$icon .= '<a style="padding-left: 5px;" href="links.php?op=edit&amp;lid=' . $lid . '">' . $imagearray['editimg'] . '</a>';
+					$icon .= '<a style="padding-left: 5px;" href="links.php?op=delete&amp;lid=' . $lid . '">' . $imagearray['deleteimg'] . '</a>';
+
+					echo '<div class="imlinks_tblrow">
+							<div class="imlinks_tblhdrcell" style="text-align: center;">' . $lid . '</div>
+							<div class="imlinks_tblcell"><a href="newlinks.php?op=edit&amp;lid=' . $lid . '">' . $title . '</a></div>
+							<div class="imlinks_tblcell" style="text-align: center;">'. $submitter . '</div>
+							<div class="imlinks_tblcell" style="text-align: center;">' . $datetime . '</div>
+							<div class="imlinks_tblcell" style="text-align: center; width: 70px;">' . $icon . '</div>
+						</div>';
+				}
+				echo '</div>';
+			} else {
+				echo '<div style="border: 1px solid #ccc; text-align: center; margin: auto; width: 99%; font-weight: bold; padding: 3px; background-color: #FFFF99;">' . _AM_IMLINKS_SUB_NOFILESWAITING . '</div>';
+			}
+
+			include_once ICMS_ROOT_PATH . '/class/pagenav.php';
+			$pagenav = new icms_view_PageNav( $new_array_count, icms::$module -> config['admin_perpage'], $start, 'start' );
+			echo '<div align="right" style="padding: 8px;">' . $pagenav -> renderNav() . '</div>';
+		}
+		icms_cp_footer();
+		break;
+}
+?>
